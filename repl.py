9919feec4a6cfae819304c4ac705e303df9d6dc6
SkyLockatf1 <g2487701324@gmail.com>
2026-04-09 13:51:30 +0800

    新增數學和隨機數函數，並實現簡化版 printf 函數的格式檢查與錯誤處理；新增 REPL 基本功能的框架

---
+++ repl.py
@@ -0,0 +1,24 @@
+def ABOUT():
+    pass
+def HELP(cmd:str):
+    pass
+def APPEND():
+    pass
+def LIST(args:list[str]):
+    if(len(args) == 0):
+        pass
+    elif(len(args) == 1):
+        pass
+    else:
+        pass
+    pass
+def INSERT(arg:str):
+    pass
+def DELETE(args:list[str]):
+    if(len(args) == 0):
+        pass
+    elif(len(args) == 1):
+        pass
+    else:
+        pass
+    pass
