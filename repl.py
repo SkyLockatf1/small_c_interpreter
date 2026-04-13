e4876748c1cfeb954ed5488210859142df9d850c
Beebee1201 <93655718+Beebee1201@users.noreply.github.com>
2026-04-14 02:42:51 +0800

    Update repl.py

--- repl.py
+++ repl.py
@@ -7,17 +7,22 @@ def APPEND():
 def LIST(args = "", buffer = []):
     if(len(buffer) == 0):
         print("buffer is empty")
+        return
     else:
         if args == "":
             for i in range(len(buffer)):
                 print("["+str(i+1)+"]: "+buffer[i])
+                return
         else:     
             args = list(map(int,args.split("-")))
-            if args[0] < 1 or args[1] > len(buffer): raise RuntimeError(f"Runtime error: Index out of bounds. Valid range is 1 to {len(buffer)}")
-
             if len(args) == 1:
+                if args[0] < 1 or args[0] > len(buffer):
+                    raise RuntimeError(f"Runtime error: Index {args[0]} out of bounds.")       
+                        
                 print("["+str(args[0])+"]: "+buffer[args[0]])
+
             elif len(args) == 2:
+                if args[0] < 1 or args[1] > len(buffer): raise RuntimeError(f"Runtime error: Index out of bounds. Valid range is 1 to {len(buffer)}")
                 for i in range(args[0], args[1]):
                     print("["+str(i)+"]: "+buffer[i])
             else:
